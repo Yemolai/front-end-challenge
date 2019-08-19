51bbaedf403b4c415a69d4f66fdd984b0061ac5f
Gabriel R <yemolai@gmail.com>
2019-08-18 23:45:40 -0300

    isEqual test

--- src/app/types/todo.spec.ts
+++ src/app/types/todo.spec.ts
@@ -4,6 +4,7 @@ describe('Todo', () => {
   it('should create an instance', () => {
     expect(new Todo(`text`)).toBeTruthy();
   });
+
   it('should have text and done prop', () => {
     const todoA = new Todo(`task`)
     expect(todoA.done).toEqual(false)
@@ -12,8 +13,18 @@ describe('Todo', () => {
     expect(todoB.text).toEqual(`task b`)
     expect(todoB.done).toEqual(true)
   })
+
   it('should have timestamp', () => {
     const todo = new Todo(`test task`)
     expect(todo.createdAt).toBeDefined()
+  })
+
+  it('should compare objects', () => {
+    const todoA = new Todo(`test 1`);
+    const todoB = new Todo(`test 2`);
+    const todoC = new Todo(todoA.text, todoA.done);
+    todoC.createdAt = todoA.createdAt;
+    expect(todoA.isEqual(todoB)).toBeFalsy();
+    expect(todoA.isEqual(todoC)).toBeTruthy();
   })
 });
